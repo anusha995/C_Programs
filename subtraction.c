8f98a05a5158a58830e2483c5d7b488c6075da3e
anusha <anusha.is007@gmail.com>
2021-05-03 16:09:51 +0000

    Adding c program files to git repo

---
+++ subtraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter the first number: ");
+    scanf("%d",&num1);
+    printf("Enter the second number: ");
+    scanf("%d",&num2);
+    int sub=num2-num1;
+    printf("Differences of two numbers are: %d\n",sub);
+    getch();
+    return 0;
+}
